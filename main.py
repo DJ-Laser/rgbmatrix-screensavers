9b91d2adc0c56ffc0fcc796db5144be8d7914b73
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2025-02-01 00:44:28 -0500

    refactor to be more oop

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from abc import ABCMeta, abstractmethod
 import random
 import time
 import math
@@ -8,9 +9,10 @@ import framebufferio
 import rgbmatrix
 
 class Buffer:
-    def __init__(self, bitmap, group):
-        self.bitmap = bitmap
-        self.group = group
+    def __init__(self, bitmap, palette):
+        self._group = displayio.Group()
+        self._bitmap = bitmap
+        self.swap_palette(palette)
 
     @property
     def width(self):
@@ -20,75 +22,67 @@ class Buffer:
     def height(self):
         return self.bitmap.height
 
+    @property
+    def group(self):
+        return self._group
+
     def fill(self, value):
-        self.bitmap.fill(value)
+        self._bitmap.fill(value)
 
     def __getitem__(self, index):
-        return self.bitmap[index]
+        return self._bitmap[index]
 
     def __setitem__(self, index, value):
-        self.bitmap[index] = value
+        self._bitmap[index] = value
 
-class Screensaver:
-    def __init__(self, display):
-        self.reset()
+    def swap_palette(self, palette):
+        grid = displayio.TileGrid(self._bitmap, pixel_shader=palette)
+        if len(self._group) > 0:
+            self._group.pop()
+        
+        self._group.append(grid)
 
-    def reset(self):
-        self.display = display
-        self.palette = self._get_palette()
-        self._buffer1 = self._setup_buffer(root=True)
-        self._buffer2 = self._setup_buffer()
-        self._useSecondaryBuffer = False
-        self._reset_buffer(self._buffer1)
-        self._reset_buffer(self._buffer2)
+class Screensaver(metaclass=ABCMeta):
+    def __init__(self):
+        self._viewport_width = 0
+        self._viewport_height = 0
     
     @property
     def width(self):
-        return self.display.width
+        return self._viewport_width
 
     @property
     def height(self):
-        return self.display.height
-    
-    def _setup_buffer(self, root=False):
-        bitmap = displayio.Bitmap(self.width, self.height, len(self.palette))
-    
-        grid = displayio.TileGrid(bitmap, pixel_shader=self.palette)
-        group = displayio.Group()
-        group.append(grid)
-    
-        if root:
-            self.display.root_group = group
-    
-        return Buffer(bitmap, group)
+        return self._viewport_height
 
-    def _get_palette(self):
+    def is_viewport_valid(self):
+        return self.width > 0 and self.height > 0
+
+    def reset_viewport(self, width, height):
+        self._viewport_width = width
+        self._viewport_height = height
+        self.reset()
+
+    @abstractmethod
+    def reset(self):
         pass
-    
-    def _reset_buffer(self, buffer):
-        pass
-    
+
+    @abstractmethod
+    def get_palette(self):
+        return displayio.Palette(0)
+
+    def reset_buffer(self, buffer):
+        buffer.fill(0)
+
+    @abstractmethod
     def draw(self, prev_buffer, current_buffer):
         pass
 
-    def run(self):
-        display.refresh()
-        
-        if self._useSecondaryBuffer:
-            self.display.root_group = self._buffer1.group
-            self.draw(self._buffer1, self._buffer2)
-        else:
-            display.root_group = self._buffer2.group
-            self.draw(self._buffer2, self._buffer1)
-        
-        self._useSecondaryBuffer = not self._useSecondaryBuffer
-
 class MatrixScreensaver(Screensaver):
-    def __init__(self, display):
-        super().__init__(display)
-        self.recent_rows = [] 
-
-    def _get_palette(self):
+    def reset(self):
+        self.recent_rows = []
+        
+    def get_palette(self):
         num_colors = 16
         palette = displayio.Palette(num_colors)
     
@@ -99,9 +93,6 @@ class MatrixScreensaver(Screensaver):
             palette[i] = (0, green_value, 0)
 
         return palette
-
-    def _reset_buffer(self, buffer):
-        buffer.fill(0)
     
     def draw(self, prev_buffer, current_buffer):
         current_buffer.fill(0)
@@ -134,39 +125,35 @@ class MatrixScreensaver(Screensaver):
         if random.random() < 0.4:
             current_buffer[(0, y)] = 15
 
-class PipesScreenSaver(Screensaver):
-    def __init__(self, display):
-        super().__init__(display)
+class PipesScreensaver(Screensaver):
+    colors = [
+        0x000000,
+        0xe0e0e0,
+        0xcc6666,
+        0xde935f,
+        0xf0c674,
+        0xb5bd68,
+        0x8abeb7,
+        0x81a2be,
+        0xb294bb,
+    ]
+    
+    def reset(self):
         self.num_pipes = 0
         self.new_pipe()
 
-    def _get_palette(self):
-        colors = [
-            0x000000,
-            0xe0e0e0,
-            0xcc6666,
-            0xde935f,
-            0xf0c674,
-            0xb5bd68,
-            0x8abeb7,
-            0x81a2be,
-            0xb294bb,
-        ]
-
-        palette = displayio.Palette(len(colors))
-        for i, color in enumerate(colors):
+    def get_palette(self):
+        palette = displayio.Palette(len(self.colors))
+        for i, color in enumerate(self.colors):
             palette[i] = color
 
         return palette
-    
-    def _reset_buffer(self, buffer):
-        buffer.fill(0)
 
     def new_pipe(self):
         self.ticks_without_turn = 0
         self.num_pipes += 1
         
-        self.color = random.randint(1, len(self.palette) - 1)
+        self.color = random.randint(1, len(self.colors) - 1)
         self.direction = random.randint(0, 3)
 
         if self.direction == 0:
@@ -214,23 +201,93 @@ class PipesScreenSaver(Screensaver):
         if x < 0 or x >= self.width or y < 0 or y >= self.height:
             self.new_pipe()
 
-screensaver_type = -1;
-def new_screensaver(display):
-    global screensaver_type
-    
-    while True:
-        new_screensaver = random.randint(0, 1)
-        if new_screensaver != screensaver_type:
-            screensaver_type = new_screensaver
-            break
+class ScreensaverManager:
+    def __init__(self, display):
+        self.screensavers = []
+        self._current_screensaver = -1
+        self._display = display
 
-    screensaver = None
-    if screensaver_type == 0:
-        screensaver = MatrixScreensaver(display)
-    elif screensaver_type == 1:
-        screensaver = PipesScreenSaver(display)
+        self._buffer1 = self._setup_buffer()
+        self._buffer2 = self._setup_buffer()
+        self._use_secondary_buffer = False
+
+        self.reset()
+
+    @property
+    def current_screensaver(self):
+        if 0 <= self._current_screensaver < len(self.screensavers):
+            return self.screensavers[self._current_screensaver]
+
+        return None
+
+    def _setup_buffer(self):
+        bitmap = displayio.Bitmap(self._display.width, self._display.height, 16)
     
-    return screensaver
+        return Buffer(bitmap, displayio.Palette(0))
+
+    def reset(self):
+        if self.current_screensaver == None:
+            return
+        
+        screensaver = self.current_screensaver
+        
+        screensaver.reset_viewport(
+            width=self._display.width,
+            height=self._display.height,
+        )
+
+        palette = screensaver.get_palette()
+        self._buffer1.swap_palette(palette)
+        self._buffer2.swap_palette(palette)
+
+        screensaver.reset_buffer(self._buffer1)
+        screensaver.reset_buffer(self._buffer2)
+
+        self._display.root_group = self._buffer1.group
+        self._use_secondary_buffer = False
+        self._display.refresh()
+
+    def add(self, screensaver):
+        self.screensavers.append(screensaver)
+
+        # First item
+        if len(self.screensavers) == 1:
+            self.cycle()
+    
+    def cycle(self):
+        num_screensavers = len(self.screensavers)
+        
+        if num_screensavers == 0:
+            return
+        elif num_screensavers == 1:
+            self._current_screensaver = 0
+            return
+        
+        while True:
+            new_screensaver = random.randint(0, num_screensavers - 1)
+            if new_screensaver != self._current_screensaver:
+                self._current_screensaver = new_screensaver
+                break
+
+        self.reset()
+
+    def run(self):
+        if self.current_screensaver == None:
+            return
+        elif not self.current_screensaver.is_viewport_valid():
+            self.reset()
+            return
+
+        print(self._buffer1[0])
+        self._display.refresh()
+        if self._use_secondary_buffer:
+            self._display.root_group = self._buffer1.group
+            self.current_screensaver.draw(self._buffer1, self._buffer2)
+        else:
+            display._root_group = self._buffer2.group
+            self.current_screensaver.draw(self._buffer2, self._buffer1)
+        
+        self._use_secondary_buffer = not self._use_secondary_buffer
 
 displayio.release_displays()
 
@@ -241,11 +298,14 @@ matrix = rgbmatrix.RGBMatrix(
     clock_pin=board.D13, latch_pin=board.D0, output_enable_pin=board.D1)
 
 display = framebufferio.FramebufferDisplay(matrix, auto_refresh=False)
-screensaver = new_screensaver(display)
+
+screensavers = ScreensaverManager(display)
+screensavers.add(MatrixScreensaver())
+screensavers.add(PipesScreensaver())
+screensavers.cycle()
 
 while True:
     for _ in range(500):
-        screensaver.run()
+        screensavers.run()
     
-    screensaver = new_screensaver(display)
-
+    screensavers.cycle()
